aeb89964f969ad4dd49d4296a0d565b3684dbae5
hongpoleon <milescm@nate.com>
2019-04-19 20:40:39 +0900

    Add @Autowired

--- RCLOL/src/main/java/org/cnu/realcoding/RCLOL/RCLOL/controller/LOLController.java
+++ RCLOL/src/main/java/org/cnu/realcoding/RCLOL/RCLOL/controller/LOLController.java
@@ -3,12 +3,14 @@ package org.cnu.realcoding.RCLOL.RCLOL.controller;
 
 import org.cnu.realcoding.RCLOL.RCLOL.Domain.Champion;
 import org.cnu.realcoding.RCLOL.RCLOL.Service.RCService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class LOLController {
+    @Autowired
     private RCService rcService;
 
 
