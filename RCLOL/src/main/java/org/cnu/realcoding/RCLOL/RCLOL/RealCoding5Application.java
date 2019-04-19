bedb90bb536dc747e9a555194acd6bb48b618b04
hongpoleon <milescm@nate.com>
2019-04-19 20:05:36 +0900

    Add RCLOL project

---
+++ RCLOL/src/main/java/org/cnu/realcoding/RCLOL/RCLOL/RealCoding5Application.java
@@ -0,0 +1,13 @@
+package org.cnu.realcoding.RCLOL.RCLOL;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealCoding5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RealCoding5Application.class, args);
+	}
+
+}
---
+++ RCLOL/src/test/java/org/cnu/realcoding/RCLOL/RCLOL/RealCoding5ApplicationTests.java
@@ -0,0 +1,16 @@
+package org.cnu.realcoding.RCLOL.RCLOL;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RealCoding5ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
