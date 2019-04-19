7f69304ab60f461aaa5042b07c75346d61728886
hongpoleon <milescm@nate.com>
2019-04-19 20:40:01 +0900

    Insert Champion information using object

--- RCLOL/src/main/java/org/cnu/realcoding/RCLOL/RCLOL/controller/LOLController.java
+++ RCLOL/src/main/java/org/cnu/realcoding/RCLOL/RCLOL/controller/LOLController.java
@@ -2,14 +2,20 @@ package org.cnu.realcoding.RCLOL.RCLOL.controller;
 
 
 import org.cnu.realcoding.RCLOL.RCLOL.Domain.Champion;
+import org.cnu.realcoding.RCLOL.RCLOL.Service.RCService;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class LOLController {
+    private RCService rcService;
+
+
     @PostMapping("/Champions")
     public void insertChampion(@RequestParam Champion champ){
-        
+
+        rcService.insertChampion(champ);
+
     }
 }
