ec3374a0a94c15f98c279f238e7c299947b0e9d0
hongpoleon <milescm@nate.com>
2019-04-19 20:38:34 +0900

    Using RequestParam

--- RCLOL/src/main/java/org/cnu/realcoding/RCLOL/RCLOL/controller/LOLController.java
+++ RCLOL/src/main/java/org/cnu/realcoding/RCLOL/RCLOL/controller/LOLController.java
@@ -1,8 +1,15 @@
 package org.cnu.realcoding.RCLOL.RCLOL.controller;
 
 
+import org.cnu.realcoding.RCLOL.RCLOL.Domain.Champion;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class LOLController {
+    @PostMapping("/Champions")
+    public void insertChampion(@RequestParam Champion champ){
+        
+    }
 }
