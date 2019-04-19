96e74b98513a2651ccebee6acfc53d3004c69993
hongpoleon <milescm@nate.com>
2019-04-19 20:44:46 +0900

    Add Get method for return all champions

--- RCLOL/src/main/java/org/cnu/realcoding/RCLOL/RCLOL/controller/LOLController.java
+++ RCLOL/src/main/java/org/cnu/realcoding/RCLOL/RCLOL/controller/LOLController.java
@@ -4,9 +4,12 @@ package org.cnu.realcoding.RCLOL.RCLOL.controller;
 import org.cnu.realcoding.RCLOL.RCLOL.Domain.Champion;
 import org.cnu.realcoding.RCLOL.RCLOL.Service.RCService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class LOLController {
@@ -19,5 +22,10 @@ public class LOLController {
 
         rcService.insertChampion(champ);
 
+    }
+
+    @GetMapping("/Champions")
+    public List<Champion> getAllChampions(){
+    return rcService.getAllChampions();
     }
 }
