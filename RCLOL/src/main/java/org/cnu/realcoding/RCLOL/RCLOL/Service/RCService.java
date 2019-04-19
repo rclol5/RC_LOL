1f6f704004c285546ae823fb7fb78a876f5d1f9b
hongpoleon <milescm@nate.com>
2019-04-19 20:36:03 +0900

    Add List in RCService class

--- RCLOL/src/main/java/org/cnu/realcoding/RCLOL/RCLOL/Service/RCService.java
+++ RCLOL/src/main/java/org/cnu/realcoding/RCLOL/RCLOL/Service/RCService.java
@@ -1,8 +1,14 @@
 package org.cnu.realcoding.RCLOL.RCLOL.Service;
 
 
+import org.cnu.realcoding.RCLOL.RCLOL.Domain.Champion;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class RCService {
+
+    private List<Champion> ChampionList = new ArrayList<>();
 }
