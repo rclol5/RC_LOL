c5e5410844ae3100b06ff20324b665ff834b87fe
hongpoleon <milescm@nate.com>
2019-05-03 16:21:08 +0900

    Make CurrentSummoner class

--- RCLOL/src/main/java/org/cnu/realcoding/RCLOL/RCLOL/Domain/CurrentSummoner.java
+++ RCLOL/src/main/java/org/cnu/realcoding/RCLOL/RCLOL/Domain/CurrentSummoner.java
@@ -1,4 +1,17 @@
 package org.cnu.realcoding.RCLOL.RCLOL.Domain;
 
+
+import lombok.Data;
+
+@Data
 public class CurrentSummoner {
+
+    private int profileId;
+    private String name;
+    private String pu_uid;
+    private long summonerLevel;
+    private long revisionDate;
+    private String id;
+    private String accountId;
+
 }
