69cb1239043835afd0a17e7b2e380164de0809f6
hongpoleon <milescm@nate.com>
2019-05-03 17:53:07 +0900

    Return RestTemplate Object

--- RCLOL/src/main/java/org/cnu/realcoding/RCLOL/RCLOL/config/LoLConfig.java
+++ RCLOL/src/main/java/org/cnu/realcoding/RCLOL/RCLOL/config/LoLConfig.java
@@ -1,4 +1,16 @@
 package org.cnu.realcoding.RCLOL.RCLOL.config;
 
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
 public class LoLConfig {
+
+
+    @Bean
+    public RestTemplate createRestTemplate(){
+        return new RestTemplate();
+    }
 }
