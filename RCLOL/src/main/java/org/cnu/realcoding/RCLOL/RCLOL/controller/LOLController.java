39ba5cd53675676380d5af06ed2c9c6ff9910dbe
hongpoleon <milescm@nate.com>
2019-05-03 17:53:52 +0900

    Use PostMapping for return League Information

--- RCLOL/src/main/java/org/cnu/realcoding/RCLOL/RCLOL/controller/LOLController.java
+++ RCLOL/src/main/java/org/cnu/realcoding/RCLOL/RCLOL/controller/LOLController.java
@@ -1,43 +1,58 @@
 package org.cnu.realcoding.RCLOL.RCLOL.controller;
 
 
-import org.cnu.realcoding.RCLOL.RCLOL.Domain.Champion;
+import org.cnu.realcoding.RCLOL.RCLOL.Domain.CurrentLeague;
 import org.cnu.realcoding.RCLOL.RCLOL.Service.RCService;
+import org.cnu.realcoding.RCLOL.RCLOL.api.LeagueApiClient;
+import org.cnu.realcoding.RCLOL.RCLOL.api.OpenRiotGamesApiClient;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class LOLController {
     @Autowired
     private RCService rcService;
 
+    LeagueApiClient lll = new LeagueApiClient();
+    OpenRiotGamesApiClient asd = new OpenRiotGamesApiClient();
 
-    @PostMapping("/Champions")
-    public void insertChampion(@RequestParam Champion champ){
+    @PostMapping("/SummonerId/{name}")
+    public CurrentLeague get(@RequestParam String name){
+        String id =  asd.getCurrentSummoner(name).getId();
+       return lll.getLeagueInformation(id);
 
-        rcService.insertChampion(champ);
 
     }
 
-    @GetMapping("/Champions")
-    public List<Champion> getAllChampions(){
-    return rcService.getAllChampions();
-    }
 
-    @GetMapping("/Champions/{name}")
-    public Champion getChampionByName(@PathVariable String name){
-        return rcService.getChampionsByName(name);
-    }
 
-    @PutMapping("/Champions")
-    public void updateChampion(@RequestBody Champion champ){
-        rcService.updateChampion(champ);
-    }
-
-    @DeleteMapping("/Champions")
-    public void removeChampion(@RequestParam String name){
-        rcService.removeChampion(name);
-    }
+//
+//    @PostMapping("/Champions")
+//    public void insertChampion(@RequestParam Champion champ){
+//
+//        rcService.insertChampion(champ);
+//
+//    }
+//
+//    @GetMapping("/Champions")
+//    public List<Champion> getAllChampions(){
+//    return rcService.getAllChampions();
+//    }
+//
+//    @GetMapping("/Champions/{name}")
+//    public Champion getChampionByName(@PathVariable String name){
+//        return rcService.getChampionsByName(name);
+//    }
+//
+//    @PutMapping("/Champions")
+//    public void updateChampion(@RequestBody Champion champ){
+//        rcService.updateChampion(champ);
+//    }
+//
+//    @DeleteMapping("/Champions")
+//    public void removeChampion(@RequestParam String name){
+//        rcService.removeChampion(name);
+//    }
 }
