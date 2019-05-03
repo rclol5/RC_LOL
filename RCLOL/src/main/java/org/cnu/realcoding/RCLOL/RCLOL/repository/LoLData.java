1892addb58cd9f254d0ddde259b707198cc2abb7
hongpoleon <milescm@nate.com>
2019-05-03 18:00:24 +0900

    Add repository for saving LoL Summoner and League Data

--- RCLOL/src/main/java/org/cnu/realcoding/RCLOL/RCLOL/repository/LoLData.java
+++ RCLOL/src/main/java/org/cnu/realcoding/RCLOL/RCLOL/repository/LoLData.java
@@ -1,4 +1,22 @@
 package org.cnu.realcoding.RCLOL.RCLOL.repository;
 
+
+import org.cnu.realcoding.RCLOL.RCLOL.Domain.CurrentSummoner;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.stereotype.Repository;
+
+@Repository
 public class LoLData {
+
+    @Autowired
+    private MongoTemplate mongoTemplate;
+
+
+    public void InsertData(CurrentSummoner encryptedSummonerID){
+
+        this.mongoTemplate.insert(encryptedSummonerID);
+    }
+
+
 }
